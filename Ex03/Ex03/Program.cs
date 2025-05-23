d09c4d760f0792c97e8d63e206d1f2e5acc7d338
TsioTheCorruptor <98714073+TsioTheCorruptor@users.noreply.github.com>
2025-05-24 02:37:14 +0300

    UI progress(accidently in main)

--- Ex03/Ex03/Program.cs
+++ Ex03/Ex03/Program.cs
@@ -1,9 +1,10 @@
-﻿
+﻿using UI;
 internal class Program
 {
 static void Main()
 {
-
+UI.Ui ui=new Ui();
+        ui.AddVehicle();
 }
 }
 
--- Ex03/Ex03/Ui.cs
+++ Ex03/Ex03/Ui.cs
@@ -1,13 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UI
 {
     internal class Ui
     {
+        private enum e_UserOptions { ReadVehicleDataFromFile, AddVehicle }
 
+        private enum e_VehicleTypes
+        {
+            FuelCar,
+            ElectricCar,
+            FuelMotorcycle,
+            ElectricMotorcycle,
+            Truck
+        }
+
+        // Keeps the same name, but now it's dynamically generated from the enum
+        private readonly string[] m_vehicleTypesToEnter = Enum.GetNames(typeof(e_VehicleTypes));
+
+        private readonly string[] m_userOptionsStrings = { "not yet", "Add car to garage" };
+
+        private void optionSelect(e_UserOptions option)
+        {
+            switch (option)
+            {
+                case e_UserOptions.ReadVehicleDataFromFile:
+                    // Add logic here
+                    break;
+                case e_UserOptions.AddVehicle:
+                    AddVehicle();
+                    break;
+            }
+        }
+
+        private void printUserOptions()
+        {
+            for (int i = 0; i < m_userOptionsStrings.Length; i++)
+            {
+                Console.WriteLine("{0}-{1}", i, m_userOptionsStrings[i]);
+            }
+        }
+
+        private void printVehicleTypesToChoose()
+        {
+            Console.WriteLine("Enter your Vehicle type:");
+            for (int i = 0; i < m_vehicleTypesToEnter.Length; i++)
+            {
+                Console.WriteLine("{0}|{1}", i, m_vehicleTypesToEnter[i]);
+            }
+        }
+
+        public void AddVehicle()
+        {
+            bool validNameIndex = true;
+            string? plateNumber;
+            int vehicleNameIndex;
+
+            Console.WriteLine("Enter license plate number:");
+            plateNumber = Console.ReadLine();
+
+            if (false) // TODO: check if already exists
+            {
+                // handle duplicate
+            }
+            else
+            {
+                printVehicleTypesToChoose();
+                validNameIndex = int.TryParse(Console.ReadLine(), out vehicleNameIndex);
+
+                while (validNameIndex == false || vehicleNameIndex >= m_vehicleTypesToEnter.Length || vehicleNameIndex < 0)
+                {
+                    Console.WriteLine("Number must be between 0 to {0}", m_vehicleTypesToEnter.Length - 1);
+                    printVehicleTypesToChoose();
+                    validNameIndex = int.TryParse(Console.ReadLine(), out vehicleNameIndex);
+                }
+
+                e_VehicleTypes selectedType = (e_VehicleTypes)vehicleNameIndex;
+                switch(selectedType)
+                {
+                    case e_VehicleTypes.ElectricCar:
+                        break;
+                    case e_VehicleTypes.ElectricMotorcycle:
+                        break;
+                    case e_VehicleTypes.FuelCar:
+                        break;
+                    case e_VehicleTypes.FuelMotorcycle:
+                        break;
+                    case e_VehicleTypes.Truck:
+                        break;
+                }
+
+                // TODO: Use selectedType in further logic
+            }
+        }
     }
 }
