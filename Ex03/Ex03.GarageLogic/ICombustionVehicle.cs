cfae00980c516ae7f0be74a54fc774cc53f3105d
Daniel <danielpbea@gmail.com>
2025-05-24 02:23:11 +0300

    added some of the back-end Logic

---
+++ Ex03/Ex03.GarageLogic/ICombustionVehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal interface ICombustionVehicle
+    {
+        public void Fuel(string i_fuelType, float i_literFuelAmount);
+    }
+}
