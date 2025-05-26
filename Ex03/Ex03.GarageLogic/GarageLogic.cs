8f5347e81e09a9c7ee73c9442e314bb33a60d06f
TsioTheCorruptor <98714073+TsioTheCorruptor@users.noreply.github.com>
2025-05-26 23:12:22 +0300

    before ui changes

--- Ex03/Ex03.GarageLogic/GarageLogic.cs
+++ Ex03/Ex03.GarageLogic/GarageLogic.cs
@@ -8,10 +8,27 @@ namespace Ex03.GarageLogic
 {
     public class GarageLogic
     {
+        
        public void SetCreatedVehicleAttribute(e_VehicleDataKeys key, string value)
         {
 
         }
-       
+        public int GetAmountOfWheelsInCurrentCar()
+        {
+            return 1;
+        }
+        public void CreateVehicle(string i_licenseNumber,string i_VehicleType)
+        {
+
+        }
+       public string getCarEnergyType()
+        {
+            return "Fuel";
+        }
+        public e_VehicleDataKeys[] GetPropertiesInfo()
+        {
+            return new e_VehicleDataKeys[] { e_VehicleDataKeys.IsCarryingDangerousMaterials,e_VehicleDataKeys.Color };
+        }
+        
     }
 }
